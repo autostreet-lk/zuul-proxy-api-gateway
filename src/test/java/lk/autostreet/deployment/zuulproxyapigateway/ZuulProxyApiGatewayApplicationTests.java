8a1d5b44c9ac0f24c1230aea8897e2ded273363f
Chathuranga Tennakoon <chathuranga@cyrup.sg>
2019-06-23 23:54:13 +0530

    first mapping added for the location service

---
+++ src/test/java/lk/autostreet/deployment/zuulproxyapigateway/ZuulProxyApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package lk.autostreet.deployment.zuulproxyapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulProxyApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
